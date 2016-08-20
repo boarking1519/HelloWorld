9e6919bdc11d006e666cb04d4d201dc9a945a6b7
boarking <boarking1519wjc@gmail.com>
2016-08-21 01:09:18 +0800

    Initial commit

---
+++ src/boarking/MyHelloWorld.java
@@ -0,0 +1,12 @@
+package boarking;
+
+/**
+ * Created by Think on 2016/8/20.
+ */
+public class MyHelloWorld {
+    public static void main(String args[]){
+        System.out.println("HelloWorld!");
+    }
+
+
+}
